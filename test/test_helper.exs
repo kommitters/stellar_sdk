4608dc85e5b46466b63ceb43fcb5323e6534c8c1
Juan Hurtado <juan@kommit.co>
2021-08-12 17:27:26 -0500

    Add test suite

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,5 @@
+# Mocks
+Mox.defmock(StellarSDK.Horizon.HackneyMock, for: StellarSDK.Horizon.Client)
+Mox.defmock(StellarSDK.Horizon.HTTPoitionMock, for: StellarSDK.Horizon.Client)
+
 ExUnit.start()
