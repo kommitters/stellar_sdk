2f2e9d31a058c6bfeb31caea0035071350897d3a
Juan Hurtado <juanmahurtado10@gmail.com>
2021-08-18 08:38:14 -0500

    Initial SDK release (#6)
    * Add a default hackney implementation
    
    * Remove :hackney wrapper module
    
    * Add library docs
    
    * Add library information and required dependencies
    
    * Add CI with github actions

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Changelog
+
+## 0.1.0 (13.08.2021)
+* Horizon HTTP client
--- mix.exs
+++ mix.exs
@@ -1,13 +1,28 @@
 defmodule StellarSDK.MixProject do
   use Mix.Project
 
+  @github_url "https://github.com/kommitters/stellar_sdk"
+  @version "0.1.0"
+
   def project do
     [
       app: :stellar_sdk,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "Elixir Stellar SDK",
+      description: description(),
+      source_url: @github_url,
+      package: package(),
+      docs: docs(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -22,8 +37,49 @@ defmodule StellarSDK.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:stellar_base, "~> 0.1.0"},
       {:hackney, "~> 1.17", optional: true},
-      {:mox, "~> 1.0", only: :test}
+      {:mox, "~> 1.0", only: :test},
+      {:excoveralls, "~> 0.14", only: :test},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description do
+    """
+    Elixir library that provides a client for interfacing with Horizon server REST endpoints.
+    """
+  end
+
+  defp package do
+    [
+      description: description(),
+      files: ["lib", "config", "mix.exs", "README*", "LICENSE"],
+      licenses: ["MIT"],
+      links: %{
+        "Changelog" => "#{@github_url}/blob/master/CHANGELOG.md",
+        "GitHub" => @github_url,
+        "Sponsor" => "https://github.com/sponsors/kommitters"
+      }
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      name: "Elixir Stellar SDK",
+      source_ref: "v#{@version}",
+      source_url: @github_url,
+      canonical: "http://hexdocs.pm/stellar_sdk",
+      extras: ["README.md", "CHANGELOG.md", "CONTRIBUTING.md"],
+      groups_for_modules: groups_for_modules()
+    ]
+  end
+
+  defp groups_for_modules do
+    [
+      Horizon: ~r/^StellarSDK\.Horizon\./
     ]
   end
 end
