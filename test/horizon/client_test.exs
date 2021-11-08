973d7ca00fb342422f52b5b210cc21cddb85ffac
Juan Hurtado <juan@kommit.co>
2021-11-08 18:41:31 -0500

    Improve CannedImpl tests for HorizonClient

--- test/horizon/client_test.exs
+++ test/horizon/client_test.exs
@@ -1,48 +1,22 @@
 defmodule Stellar.Horizon.Client.CannedClientImpl do
   @moduledoc false
 
-  import Stellar.Test.Fixtures, only: [account_data: 1]
-
   @behaviour Stellar.Horizon.Client.Spec
 
   @impl true
-  def request(:get, "/accounts/123", _headers, _body, _opts) do
-    {:ok, %{status_code: 404, body: ~s<{"status": 404, "title": "Resource Missing"}>}}
-  end
-
-  def request(:get, "/accounts/" <> id, _headers, _body, _opts) do
-    {:ok, %{status_code: 200, body: account_data(id)}}
-  end
-
-  def request(:delete, _path, _headers, _body, _opts) do
-    {:error, %{reason: "error"}}
+  def request(_method, _path, _headers, _body, _opts) do
+    send(self(), {:requested, 200})
+    {:ok, 200, [], nil}
   end
 end
 
 defmodule Stellar.Horizon.ClientTest do
   use ExUnit.Case
 
-  import Stellar.Test.Fixtures, only: [account_data: 1]
-
   alias Stellar.Horizon.Client
 
-  setup do
-    %{
-      body: account_data("GAYOLLLUIZE4DZMBB2ZBKGBUBZLIOYU6XBNXCW4BVA"),
-      body_404: ~s<{"status": 404, "title": "Resource Missing"}>
-    }
-  end
-
-  test "request/5 success", %{body: body} do
-    {:ok, %{status_code: 200, body: ^body}} =
-      Client.request(:get, "/accounts/GAYOLLLUIZE4DZMBB2ZBKGBUBZLIOYU6XBNXCW4BVA")
-  end
-
-  test "request/5 not_found", %{body_404: body} do
-    {:ok, %{status_code: 404, body: ^body}} = Client.request(:get, "/accounts/123")
-  end
-
-  test "request/5 error" do
-    {:error, %{reason: "error"}} = Client.request(:delete, "/accounts")
+  test "request/5" do
+    Client.request(:get, "/accounts/GAYOLLLUIZE4DZMBB2ZBKGBUBZLIOYU6XBNXCW4BVA")
+    assert_receive({:requested, 200})
   end
 end
