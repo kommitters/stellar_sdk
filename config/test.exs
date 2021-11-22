cfe0e68969cd935b16998d0ed88c990507cdda35
Juan Hurtado <juan@kommit.co>
2021-11-22 17:10:54 -0500

    Add tx_build tests

--- config/test.exs
+++ config/test.exs
@@ -3,5 +3,4 @@ use Mix.Config
 config :stellar_sdk,
   network: :test,
   http_client_impl: Stellar.Horizon.Client.CannedClientImpl,
-  http_client: Stellar.Horizon.Client.CannedHTTPClient,
-  keypair_impl: Stellar.KeyPair.CannedKeyPairImpl
+  http_client: Stellar.Horizon.Client.CannedHTTPClient
