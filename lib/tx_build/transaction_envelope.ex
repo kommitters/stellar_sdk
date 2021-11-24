3392d8a265bda5cffcae6b8ab95ebf673bf3b507
Juan Hurtado <juan@kommit.co>
2021-11-24 12:21:57 -0500

    Sign transactions module

--- lib/tx_build/transaction_envelope.ex
+++ lib/tx_build/transaction_envelope.ex
@@ -1,12 +1,10 @@
 defmodule Stellar.TxBuild.TransactionEnvelope do
   @moduledoc """
-  `Envelope` struct definition.
+  `TransactionEnvelope` struct definition.
   """
-  alias Stellar.TxBuild.Transaction
+  alias Stellar.TxBuild.{Signature, Transaction, TransactionSignature}
 
   alias StellarBase.XDR.{
-    DecoratedSignature,
-    DecoratedSignatures,
     EnvelopeType,
     TransactionV1Envelope,
     TransactionEnvelope
@@ -14,32 +12,31 @@ defmodule Stellar.TxBuild.TransactionEnvelope do
 
   @behaviour Stellar.TxBuild.XDR
 
-  @type signatures :: list(DecoratedSignature.t())
+  @type signatures :: list(Signature.t())
 
   @type t :: %__MODULE__{tx: Transaction.t(), signatures: signatures()}
 
   defstruct [:tx, :signatures]
 
   @impl true
-  def new(%Transaction{} = tx, signatures) do
-    %__MODULE__{
-      tx: Transaction.to_xdr(tx),
-      signatures: DecoratedSignatures.new(signatures)
-    }
+  def new(%Transaction{} = tx, signatures) when is_list(signatures) do
+    %__MODULE__{tx: tx, signatures: signatures}
   end
 
   @impl true
   def to_xdr(%__MODULE__{tx: tx, signatures: signatures}) do
     envelope_type = EnvelopeType.new(:ENVELOPE_TYPE_TX)
+    decorated_signatures = TransactionSignature.sign(tx, signatures)
 
     tx
-    |> TransactionV1Envelope.new(signatures)
+    |> Transaction.to_xdr()
+    |> TransactionV1Envelope.new(decorated_signatures)
     |> TransactionEnvelope.new(envelope_type)
   end
 
-  @spec to_base64(envelope_xdr :: TransactionEnvelope.t()) :: String.t()
-  def to_base64(%TransactionEnvelope{} = envelope_xdr) do
-    envelope_xdr
+  @spec to_base64(tx_envelope_xdr :: TransactionEnvelope.t()) :: String.t()
+  def to_base64(%TransactionEnvelope{} = tx_envelope_xdr) do
+    tx_envelope_xdr
     |> TransactionEnvelope.encode_xdr!()
     |> Base.encode64()
   end
--- lib/tx_build/tx_build.ex
+++ lib/tx_build/tx_build.ex
@@ -1,12 +1,22 @@
 defmodule Stellar.TxBuild do
   @moduledoc """
-  TxBuild implementation.
+  Transaction build implementation.
   """
-  alias Stellar.TxBuild.{Account, Memo, Operations, TimeBounds, Transaction, TransactionEnvelope}
+  alias Stellar.TxBuild.{
+    Account,
+    Memo,
+    Operations,
+    Signature,
+    TimeBounds,
+    Transaction,
+    TransactionEnvelope
+  }
+
+  @type signatures :: list(Signature.t())
 
   @type t :: %__MODULE__{
           tx: Transaction.t(),
-          signatures: list(),
+          signatures: signatures(),
           tx_envelope: TransactionEnvelope.t()
         }
 
@@ -28,7 +38,7 @@ defmodule Stellar.TxBuild do
   end
 
   @spec set_timeout(tx_build :: t(), timeout :: non_neg_integer()) :: t()
-  def set_timeout(%__MODULE__{tx: tx} = tx_build, timeout) do
+  def set_timeout(%__MODULE__{tx: tx} = tx_build, timeout) when is_integer(timeout) do
     transaction = %{tx | time_bounds: TimeBounds.set_max_time(timeout)}
     %{tx_build | tx: transaction}
   end
@@ -40,19 +50,21 @@ defmodule Stellar.TxBuild do
   end
 
   @spec sign(tx_build :: t(), keypair :: tuple()) :: t()
-  def sign(%__MODULE__{} = tx_build, _keypair) do
-    tx_build
+  def sign(%__MODULE__{signatures: signatures} = tx_build, {public_key, secret}) do
+    signature = Signature.new(public_key, secret)
+    %{tx_build | signatures: signatures ++ [signature]}
   end
 
-  @spec build(tx_build :: t()) :: TransactionEnvelope.t()
-  def build(%__MODULE__{tx: tx, signatures: signatures}) do
+  @spec build(tx_build :: t()) :: t()
+  def build(%__MODULE__{tx: tx, signatures: signatures} = tx_build) do
+    %{tx_build | tx_envelope: TransactionEnvelope.new(tx, signatures)}
+  end
+
+  @spec envelope(tx_build :: t()) :: String.t()
+  def envelope(%__MODULE__{tx: tx, signatures: signatures}) do
     tx
     |> TransactionEnvelope.new(signatures)
     |> TransactionEnvelope.to_xdr()
-  end
-
-  @spec to_base64(envelope_xdr :: struct()) :: String.t()
-  def to_base64(envelope_xdr) do
-    TransactionEnvelope.to_base64(envelope_xdr)
+    |> TransactionEnvelope.to_base64()
   end
 end
