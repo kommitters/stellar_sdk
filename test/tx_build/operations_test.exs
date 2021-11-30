6a366aefba31827fde84ca25a337b8ecaaa6670a
Juan Hurtado <juan@kommit.co>
2021-11-30 16:08:41 -0500

    Add test suite

--- test/tx_build/operations_test.exs
+++ test/tx_build/operations_test.exs
@@ -2,18 +2,24 @@ defmodule Stellar.TxBuild.OperationsTest do
   use ExUnit.Case
 
   alias StellarBase.XDR.Operations, as: OperationsXDR
-  alias Stellar.TxBuild.Operations
+  alias Stellar.TxBuild.{Operation, Operations, CreateAccount}
 
   setup do
-    %{operations: Operations.new()}
+    op_body = CreateAccount.new("GBTG2POJVVSRBQSZVA3IYJEZJQLPTIVVYOYRLTZEAEFBMVP72ZTQYA2V", 1.5)
+    operation = Operation.new(op_body)
+
+    %{
+      operation: operation,
+      operations: Operations.new()
+    }
   end
 
   test "new/2" do
     %Operations{operations: []} = Operations.new()
   end
 
-  test "add/1", %{operations: operations} do
-    %Operations{operations: [:test]} = Operations.add(operations, :test)
+  test "add/1", %{operations: operations, operation: operation} do
+    %Operations{operations: [^operation]} = Operations.add(operations, operation)
   end
 
   test "to_xdr/1", %{operations: operations} do
