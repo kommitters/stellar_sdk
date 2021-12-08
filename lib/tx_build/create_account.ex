f26aaa5ba0d97a91d9bdcf2cbff6b87dce742e35
Juan Hurtado <juan@kommit.co>
2021-12-08 16:48:10 -0500

    Refactor the CreateAccount operation

--- lib/tx_build/create_account.ex
+++ lib/tx_build/create_account.ex
@@ -3,27 +3,56 @@ defmodule Stellar.TxBuild.CreateAccount do
   `CreateAccountOp` struct definition.
   """
   alias Stellar.TxBuild.{AccountID, Amount}
-  alias StellarBase.XDR.Operations.CreateAccount
+  alias StellarBase.XDR.{OperationBody, OperationType, Operations.CreateAccount}
 
   @behaviour Stellar.TxBuild.XDR
 
-  @type t :: %__MODULE__{destination: String.t(), starting_balance: non_neg_integer()}
+  @type error :: {:error, atom()}
 
-  defstruct [:destination, :starting_balance]
+  @type source_account :: String.t() | nil
+
+  @type t :: %__MODULE__{
+          destination: AccountID.t(),
+          starting_balance: Amount.t(),
+          source_account: source_account()
+        }
+
+  defstruct [:destination, :starting_balance, :source_account]
 
   @impl true
-  def new(destination, starting_balance) do
-    %__MODULE__{
-      destination: AccountID.new(destination),
-      starting_balance: Amount.new(starting_balance)
-    }
+  def new(args, opts \\ [])
+
+  def new(args, _opts) when is_list(args) do
+    destination = Keyword.get(args, :destination)
+    starting_balance = Keyword.get(args, :starting_balance)
+    source_account = Keyword.get(args, :source_account)
+
+    with %AccountID{} = account_id <- AccountID.new(destination),
+         %Amount{} = amount <- Amount.new(starting_balance) do
+      %__MODULE__{
+        destination: account_id,
+        starting_balance: amount,
+        source_account: source_account
+      }
+    else
+      error -> error_message(error)
+    end
   end
+
+  def new(_args, _opts), do: {:error, :invalid_arguments}
 
   @impl true
   def to_xdr(%__MODULE__{destination: destination, starting_balance: starting_balance}) do
-    CreateAccount.new(
-      AccountID.to_xdr(destination),
-      Amount.to_xdr(starting_balance)
-    )
+    op_type = OperationType.new(:CREATE_ACCOUNT)
+    destination = AccountID.to_xdr(destination)
+    amount = Amount.to_xdr(starting_balance)
+
+    destination
+    |> CreateAccount.new(amount)
+    |> OperationBody.new(op_type)
   end
+
+  @spec error_message(error :: error()) :: error()
+  defp error_message({:error, :invalid_account_id}), do: {:error, :invalid_destination}
+  defp error_message({:error, :invalid_amount}), do: {:error, :invalid_starting_balance}
 end
