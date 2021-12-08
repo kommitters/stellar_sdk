67b32c70b726b5ecd66be9f1dd2f2ca5b4de6f00
Juan Hurtado <juan@kommit.co>
2021-12-08 16:47:42 -0500

    Include validations into the TxBuild construction

--- lib/tx_build/operation.ex
+++ lib/tx_build/operation.ex
@@ -3,45 +3,36 @@ defmodule Stellar.TxBuild.Operation do
   `Operation` struct definition.
   """
   alias Stellar.TxBuild.CreateAccount
-  alias StellarBase.XDR.{Operation, OperationBody, OperationType, OptionalMuxedAccount}
+  alias StellarBase.XDR.{Operation, OptionalMuxedAccount}
 
   @behaviour Stellar.TxBuild.XDR
 
-  @type op_body :: CreateAccount.t()
+  @type operation :: CreateAccount.t() | Payment.t()
 
   @type source_account :: String.t() | nil
 
-  @type t :: %__MODULE__{op_body: op_body(), source_account: source_account()}
+  @type t :: %__MODULE__{body: operation(), source_account: source_account()}
 
-  defstruct [:op_body, :source_account]
+  defstruct [:body, :source_account]
 
   @impl true
-  def new(op_body, opts \\ []) do
-    source_account = Keyword.get(opts, :source_account, nil)
-    %__MODULE__{op_body: op_body, source_account: source_account}
+  def new(body, opts \\ [])
+
+  def new(body, _opts) do
+    with :ok <- valid_operation(body),
+         do: %__MODULE__{body: body, source_account: body.source_account}
   end
 
   @impl true
-  def to_xdr(%__MODULE__{op_body: %CreateAccount{} = op_body, source_account: source_account}) do
-    op_type = OperationType.new(:CREATE_ACCOUNT)
-
-    op_body
-    |> CreateAccount.to_xdr()
-    |> operation_xdr(op_type, source_account)
-  end
-
-  def to_xdr(_operation), do: {:error, :invalid_operation}
-
-  @spec operation_xdr(
-          op_body :: struct(),
-          op_type :: struct(),
-          source_account :: source_account()
-        ) :: Operation.t()
-  defp operation_xdr(op_body, op_type, source_account) do
+  def to_xdr(%__MODULE__{body: %{__struct__: operation} = body, source_account: source_account}) do
     source_account = OptionalMuxedAccount.new(source_account)
 
-    op_body
-    |> OperationBody.new(op_type)
+    body
+    |> operation.to_xdr()
     |> Operation.new(source_account)
   end
+
+  @spec valid_operation(operation :: operation()) :: :ok | {:error, atom()}
+  defp valid_operation(%CreateAccount{}), do: :ok
+  defp valid_operation(_operation), do: {:error, :unknown_operation}
 end
