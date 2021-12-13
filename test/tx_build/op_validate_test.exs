6692e50346f3461edb17b554297d32597a54fa88
Juan Hurtado <juan@kommit.co>
2021-12-13 15:41:37 -0500

    Add PathPaymentStrictSend operation tests

--- test/tx_build/op_validate_test.exs
+++ test/tx_build/op_validate_test.exs
@@ -1,45 +1,59 @@
 defmodule Stellar.TxBuild.OpValidateTest do
   use ExUnit.Case
 
-  alias Stellar.TxBuild.{Account, AccountID, Amount, Asset, OpValidate}
+  alias Stellar.TxBuild.{Account, AccountID, Amount, Asset, AssetsPath, OpValidate}
 
   setup do
     %{account_id: "GD726E62G6G4ANHWHIQTH5LNMFVF2EQSEXITB6DZCCTKVU6EQRRE2SJS"}
   end
 
-  test "validate_account_id/2", %{account_id: account_id} do
+  test "validate_account_id/1", %{account_id: account_id} do
     {:ok, %AccountID{}} = OpValidate.validate_account_id({:destination, account_id})
   end
 
-  test "validate_account_id/2 error" do
+  test "validate_account_id/1 error" do
     {:error, [destination: :invalid_account_id]} =
       OpValidate.validate_account_id({:destination, "ABC"})
   end
 
-  test "validate_account/2", %{account_id: account_id} do
+  test "validate_account/1", %{account_id: account_id} do
     {:ok, %Account{}} = OpValidate.validate_account({:destination, account_id})
   end
 
-  test "validate_account/2 error" do
+  test "validate_account/1 error" do
     {:error, [destination: :invalid_account_id]} =
       OpValidate.validate_account_id({:destination, "ABC"})
   end
 
-  test "validate_amount/2" do
+  test "validate_amount/1" do
     {:ok, %Amount{}} = OpValidate.validate_amount({:starting_balance, 100})
   end
 
-  test "validate_amount/2 error" do
+  test "validate_amount/1 error" do
     {:error, [starting_balance: :invalid_amount]} =
       OpValidate.validate_amount({:starting_balance, "100"})
   end
 
-  test "validate_asset/2" do
+  test "validate_asset/1" do
     {:ok, %Asset{}} = OpValidate.validate_asset({:asset, :native})
   end
 
-  test "validate_asset/2 error" do
+  test "validate_asset/1 error" do
     {:error, [dest_asset: :invalid_asset_issuer]} =
       OpValidate.validate_asset({:dest_asset, {"BTCN", "ABC"}})
+  end
+
+  test "validate_optional_assets_path/1", %{account_id: account_id} do
+    {:ok, %AssetsPath{}} =
+      OpValidate.validate_optional_assets_path({:path, [:native, {"BTCN", account_id}]})
+  end
+
+  test "validate_optional_assets_path/1 empty_list" do
+    {:ok, %AssetsPath{assets: []}} = OpValidate.validate_optional_assets_path({:path, nil})
+  end
+
+  test "validate_optional_assets_path/1 error" do
+    {:error, [path: :invalid_asset_issuer]} =
+      OpValidate.validate_optional_assets_path({:path, [:native, {"BTCN", "ABC"}]})
   end
 end
---
+++ test/tx_build/path_payment_strict_send_test.exs
@@ -0,0 +1,126 @@
+defmodule Stellar.TxBuild.PathPaymentStrictSendTest do
+  use ExUnit.Case
+
+  import Stellar.Test.XDRFixtures, only: [create_path_payment_strict_send_op_xdr: 6]
+
+  alias Stellar.TxBuild.{Account, Amount, Asset, AssetsPath, PathPaymentStrictSend}
+
+  setup do
+    asset_issuer = "GBTG2POJVVSRBQSZVA3IYJEZJQLPTIVVYOYRLTZEAEFBMVP72ZTQYA2V"
+    asset_code4 = "BTCN"
+    asset_code12 = "BTCNEW2000"
+
+    destination = "GD726E62G6G4ANHWHIQTH5LNMFVF2EQSEXITB6DZCCTKVU6EQRRE2SJS"
+    send_asset = :native
+    send_amount = 100
+    dest_asset = {asset_code4, asset_issuer}
+    dest_min = 2_000
+    path = [:native, {asset_code12, asset_issuer}]
+
+    %{
+      destination: destination,
+      send_asset: send_asset,
+      send_amount: send_amount,
+      dest_asset: dest_asset,
+      dest_min: dest_min,
+      path: path,
+      xdr:
+        create_path_payment_strict_send_op_xdr(
+          destination,
+          send_asset,
+          send_amount,
+          dest_asset,
+          dest_min,
+          path
+        )
+    }
+  end
+
+  test "new/2", %{
+    destination: destination,
+    send_asset: send_asset,
+    send_amount: send_amount,
+    dest_asset: dest_asset,
+    dest_min: dest_min,
+    path: path
+  } do
+    destination_str = Account.new(destination)
+    asset_str = Asset.new(send_asset)
+    amount_str = Amount.new(send_amount)
+    path_str = AssetsPath.new(path)
+
+    %PathPaymentStrictSend{
+      destination: ^destination_str,
+      send_asset: ^asset_str,
+      send_amount: ^amount_str,
+      path: ^path_str
+    } =
+      PathPaymentStrictSend.new(
+        destination: destination,
+        send_asset: send_asset,
+        send_amount: send_amount,
+        dest_asset: dest_asset,
+        dest_min: dest_min,
+        path: path
+      )
+  end
+
+  test "new/2 with_invalid_destination" do
+    {:error, [destination: :invalid_account_id]} =
+      PathPaymentStrictSend.new(
+        destination: "ABC",
+        send_asset: :native,
+        send_amount: 100,
+        dest_asset: :native,
+        dest_min: 50
+      )
+  end
+
+  test "new/2 with_invalid_send_asset", %{destination: destination} do
+    {:error, [send_asset: :invalid_asset_issuer]} =
+      PathPaymentStrictSend.new(
+        destination: destination,
+        send_asset: {"BTCN", "BAD"},
+        send_amount: 100,
+        dest_asset: :native,
+        dest_min: 50
+      )
+  end
+
+  test "new/2 with_invalid_amount", %{destination: destination} do
+    {:error, [send_amount: :invalid_amount]} =
+      PathPaymentStrictSend.new(
+        destination: destination,
+        send_asset: :native,
+        send_amount: "100",
+        dest_asset: :native,
+        dest_min: 50
+      )
+  end
+
+  test "new/2 with_invalid_attributes" do
+    {:error, :invalid_operation_attributes} = PathPaymentStrictSend.new("ABC", "123")
+  end
+
+  test "to_xdr/1", %{
+    xdr: xdr,
+    destination: destination,
+    send_asset: send_asset,
+    send_amount: send_amount,
+    dest_asset: dest_asset,
+    dest_min: dest_min,
+    path: path
+  } do
+    ^xdr =
+      [
+        destination: destination,
+        send_asset: send_asset,
+        send_amount: send_amount,
+        dest_asset: dest_asset,
+        dest_min: dest_min,
+        path: path
+      ]
+      |> PathPaymentStrictSend.new()
+      |> PathPaymentStrictSend.to_xdr()
+  end
+end
