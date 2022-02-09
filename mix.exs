e94c62c64da56ed0408b7f5cb356e9db5b06b41f
Juan Hurtado <juan@kommit.co>
2022-02-09 17:43:57 -0500

    Bump to v0.5.0 stellar_base dep

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule Stellar.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:stellar_base, "~> 0.3.0"},
+      {:stellar_base, "~> 0.5.0"},
       {:ed25519, "~> 1.3"},
       {:hackney, "~> 1.17", optional: true},
       {:jason, "~> 1.0", optional: true},
