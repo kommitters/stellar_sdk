18d80855e8184ab36c25a5dbdad74b74016f2a34
Juan Hurtado <juan@kommit.co>
2022-02-09 17:46:35 -0500

    CAP-0027 Add support for muxed accounts

--- lib/tx_build/account.ex
+++ lib/tx_build/account.ex
@@ -3,40 +3,117 @@ defmodule Stellar.TxBuild.Account do
   `Account` struct definition.
   """
   alias Stellar.KeyPair
-  alias StellarBase.XDR.{CryptoKeyType, MuxedAccount, UInt256}
+  alias StellarBase.XDR.{CryptoKeyType, MuxedAccount, MuxedAccountMed25519, UInt64, UInt256}
 
   @behaviour Stellar.TxBuild.XDR
 
+  @type address :: String.t()
   @type account_id :: String.t()
-  @type validation :: {:ok, account_id()} | {:error, atom()}
+  @type id :: integer() | nil
+  @type type :: :ed25519_public_key | :ed25519_muxed_account
+  @type error :: {:error, atom()}
+  @type validation :: {:ok, any()} | error()
 
-  @type t :: %__MODULE__{account_id: account_id()}
+  @type t :: %__MODULE__{address: address(), account_id: account_id(), id: id(), type: type()}
 
-  defstruct [:account_id]
+  defstruct [:address, :account_id, :id, :type]
 
   @impl true
-  def new(account_id, opts \\ [])
+  def new(address, opts \\ [])
 
-  def new(account_id, _opts) do
-    with {:ok, account_id} <- validate_raw_account_id(account_id),
-         do: %__MODULE__{account_id: account_id}
+  def new(address, _opts) when byte_size(address) == 69 do
+    with {:ok, address} <- validate_muxed_address(address),
+         {:ok, {account_id, id}} <- parse_muxed_address(address) do
+      %__MODULE__{
+        address: address,
+        account_id: account_id,
+        id: id,
+        type: :ed25519_muxed_account
+      }
+    end
   end
 
-  @impl true
-  def to_xdr(%__MODULE__{account_id: account_id}) do
-    type = CryptoKeyType.new(:KEY_TYPE_ED25519)
+  def new(address, _opts) when byte_size(address) == 56 do
+    case validate_public_key(address) do
+      {:ok, account_id} ->
+        %__MODULE__{
+          address: account_id,
+          account_id: account_id,
+          type: :ed25519_public_key
+        }
 
-    account_id
-    |> KeyPair.raw_public_key()
-    |> UInt256.new()
+      error ->
+        error
+    end
+  end
+
+  def new(_address, _opts), do: {:error, :invalid_ed25519_public_key}
+
+  @impl true
+  def to_xdr(%__MODULE__{account_id: account_id, id: id, type: :ed25519_muxed_account}) do
+    type = CryptoKeyType.new(:KEY_TYPE_MUXED_ED25519)
+    ed25519_public_key_xdr = ed25519_public_key_xdr(account_id)
+
+    id
+    |> UInt64.new()
+    |> MuxedAccountMed25519.new(ed25519_public_key_xdr)
     |> MuxedAccount.new(type)
   end
 
-  @spec validate_raw_account_id(account_id :: String.t()) :: validation()
-  defp validate_raw_account_id(account_id) do
-    case KeyPair.validate_public_key(account_id) do
-      :ok -> {:ok, account_id}
-      _error -> {:error, :invalid_account_id}
+  def to_xdr(%__MODULE__{account_id: account_id}) do
+    type = CryptoKeyType.new(:KEY_TYPE_ED25519)
+    ed25519_public_key_xdr = ed25519_public_key_xdr(account_id)
+
+    MuxedAccount.new(ed25519_public_key_xdr, type)
+  end
+
+  @spec create_muxed(account_id :: account_id(), id :: id()) :: t()
+  def create_muxed(account_id, id) when byte_size(account_id) == 56 and is_integer(id) do
+    account_id
+    |> KeyPair.raw_public_key()
+    |> Kernel.<>(<<id::big-unsigned-integer-size(64)>>)
+    |> KeyPair.from_raw_muxed_account()
+    |> new()
+  end
+
+  def create_muxed(_account_id, _id), do: {:error, :invalid_muxed_account}
+
+  @spec validate_muxed_address(address :: address()) :: validation()
+  defp validate_muxed_address(address) do
+    case KeyPair.validate_muxed_account(address) do
+      :ok -> {:ok, address}
+      error -> error
     end
+  end
+
+  @spec validate_public_key(address :: address()) :: validation()
+  defp validate_public_key(address) do
+    case KeyPair.validate_public_key(address) do
+      :ok -> {:ok, address}
+      error -> error
+    end
+  end
+
+  @spec parse_muxed_address(address :: address()) :: {:ok, {account_id(), id()}} | error()
+  defp parse_muxed_address(address) do
+    address
+    |> KeyPair.raw_muxed_account()
+    |> encode_muxed_address()
+  end
+
+  @spec encode_muxed_address(decoded_address :: binary()) ::
+          {:ok, {account_id(), id()}} | error()
+  defp encode_muxed_address(<<decoded::binary-size(32), id::big-unsigned-integer-size(64)>>) do
+    ed25519_key = KeyPair.from_raw_public_key(decoded)
+    {:ok, {ed25519_key, id}}
+  end
+
+  defp encode_muxed_address(_muxed_account), do: {:error, :invalid_muxed_address}
+
+  @spec ed25519_public_key_xdr(account_id :: account_id()) :: UInt256.t()
+  defp ed25519_public_key_xdr(account_id) do
+    account_id
+    |> KeyPair.raw_public_key()
+    |> UInt256.new()
   end
 end
--- test/tx_build/account_test.exs
+++ test/tx_build/account_test.exs
@@ -1,30 +1,79 @@
 defmodule Stellar.TxBuild.AccountTest do
   use ExUnit.Case
 
-  import Stellar.Test.XDRFixtures, only: [muxed_account_xdr: 1]
+  alias Stellar.Test.Fixtures.XDR, as: XDRFixtures
   alias Stellar.TxBuild.Account
 
   setup do
-    account_id = "GD726E62G6G4ANHWHIQTH5LNMFVF2EQSEXITB6DZCCTKVU6EQRRE2SJS"
+    account_id = "GBXV5U2D67J7HUW42JKBGD4WNZON4SOPXXDFTYQ7BCOG5VCARGCRMQQH"
+    muxed_address = "MBXV5U2D67J7HUW42JKBGD4WNZON4SOPXXDFTYQ7BCOG5VCARGCRMAAAAAAAAAAAARKPQ"
 
     %{
       account_id: account_id,
-      xdr: muxed_account_xdr(account_id)
+      muxed_address: muxed_address,
+      muxed_account_xdr: XDRFixtures.muxed_account(account_id),
+      muxed_account_med25519_xdr: XDRFixtures.muxed_account_med25519(muxed_address)
     }
   end
 
-  test "new/2", %{account_id: account_id} do
-    %Account{account_id: ^account_id} = Account.new(account_id)
+  describe "new/2" do
+    test "ed25519_public_key", %{account_id: account_id} do
+      %Account{address: ^account_id, account_id: ^account_id, type: :ed25519_public_key} =
+        Account.new(account_id)
+    end
+
+    test "muxed_account", %{muxed_address: muxed_address, account_id: account_id} do
+      %Account{
+        address: ^muxed_address,
+        account_id: ^account_id,
+        id: 4,
+        type: :ed25519_muxed_account
+      } = Account.new(muxed_address)
+    end
+
+    test "invalid_ed25519_public_key" do
+      {:error, :invalid_ed25519_public_key} =
+        Account.new("GAAAAA2D67J7HUW42JKBGD4WNZON4SOPXXDFTYQ7BCOG5VCARGCRMQQH")
+    end
+
+    test "invalid_muxed_account" do
+      {:error, :invalid_ed25519_muxed_account} =
+        Account.new("MBXV5U2D67J7HUW42JKBGD4WNZON4SOPXXDFTYQ7BCOG5VCARGCRMAAAAAAAAAAAARKPD")
+    end
+
+    test "invalid_key" do
+      {:error, :invalid_ed25519_public_key} = Account.new("ABCD")
+    end
   end
 
-  test "new/2 invalid_key" do
-    {:error, :invalid_account_id} = Account.new("ABCD")
+  describe "to_xdr/1" do
+    test "ed25519_public_key", %{muxed_account_xdr: xdr, account_id: account_id} do
+      ^xdr =
+        account_id
+        |> Account.new()
+        |> Account.to_xdr()
+    end
+
+    test "muxed_account", %{muxed_account_med25519_xdr: xdr, muxed_address: muxed_address} do
+      ^xdr =
+        muxed_address
+        |> Account.new()
+        |> Account.to_xdr()
+    end
   end
 
-  test "to_xdr/1", %{xdr: xdr, account_id: account_id} do
-    ^xdr =
-      account_id
-      |> Account.new()
-      |> Account.to_xdr()
+  describe "create_muxed/2" do
+    test "success", %{muxed_address: muxed_address, account_id: account_id} do
+      %Account{
+        address: ^muxed_address,
+        account_id: ^account_id,
+        id: 4,
+        type: :ed25519_muxed_account
+      } = Account.create_muxed(account_id, 4)
+    end
+
+    test "invalid_attribute", %{account_id: account_id} do
+      {:error, :invalid_muxed_account} = Account.create_muxed(account_id, "123")
+    end
   end
 end
