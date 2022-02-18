c5b5de09a9b966d3e5b0f45a0e45fb6260bf669f
Juan Hurtado <juan@kommit.co>
2022-02-18 12:15:31 -0500

    Compose functional Horizon requests  (#100)
    * Functional Horizon requests implementation

--- lib/horizon/collection.ex
+++ lib/horizon/collection.ex
@@ -6,19 +6,14 @@ defmodule Stellar.Horizon.Collection do
 
   @type resource :: module()
   @type records :: [resource(), ...]
-  @type api_response :: map()
+  @type response :: map()
   @type link :: String.t()
-  @type args :: Keyword.t()
-  @type extra_args :: list()
-  @type query_string :: String.t()
 
   @type t :: %__MODULE__{records: records(), self: link(), prev: link(), next: link()}
 
   defstruct [:records, :self, :next, :prev]
 
-  @default_args ~w(cursor order limit)a
-
-  @spec new(response :: {resource(), api_response()}) :: t()
+  @spec new(response :: {resource(), response()}) :: t()
   def new(
         {resource,
          %{
@@ -30,22 +25,4 @@ defmodule Stellar.Horizon.Collection do
   end
 
   def new(_response), do: raise(CollectionError, :invalid_collection)
-
-  @spec query_arguments(args :: args(), extra_args :: list()) :: query_string()
-  def query_arguments(args, opts \\ []) do
-    extra_args = Keyword.get(opts, :extra_args, [])
-
-    args
-    |> Keyword.take(@default_args ++ extra_args)
-    |> Enum.reject(&is_empty_arg/1)
-    |> build_query_string()
-  end
-
-  @spec build_query_string(query :: args()) :: query_string()
-  defp build_query_string([]), do: ""
-  defp build_query_string(query), do: "?" <> URI.encode_query(query)
-
-  @spec is_empty_arg(param :: {atom(), any()}) :: boolean()
-  defp is_empty_arg({_key, nil}), do: true
-  defp is_empty_arg({_key, value}), do: to_string(value) == ""
 end
