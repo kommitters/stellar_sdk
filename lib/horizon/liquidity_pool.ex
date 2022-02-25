5c323e2b2df87491ba12fd4d6b2bd31609362eac
Juan Hurtado <juan@kommit.co>
2022-02-25 16:11:17 -0500

    Add structures for Horizon resources

---
+++ lib/horizon/liquidity_pool.ex
@@ -0,0 +1,51 @@
+defmodule Stellar.Horizon.LiquidityPool do
+  @moduledoc """
+  Represents a `LiquidityPool` resource from Horizon API.
+  """
+
+  @behaviour Stellar.Horizon.Resource
+
+  alias Stellar.Horizon.Mapping
+
+  @type t :: %__MODULE__{
+          id: String.t(),
+          paging_token: String.t(),
+          fee_bp: non_neg_integer(),
+          type: String.t(),
+          total_trustlines: non_neg_integer(),
+          total_shares: float(),
+          reserves: list(map()),
+          last_modified_ledger: non_neg_integer(),
+          last_modified_time: DateTime.t()
+        }
+
+  defstruct [
+    :id,
+    :paging_token,
+    :fee_bp,
+    :type,
+    :total_trustlines,
+    :total_shares,
+    :reserves,
+    :last_modified_ledger,
+    :last_modified_time
+  ]
+
+  @mapping [
+    fee_bp: :integer,
+    total_trustlines: :integer,
+    total_shares: :float,
+    last_modified_ledger: :integer,
+    last_modified_time: :date_time,
+    reserves: {:list, :map, [amount: :float]}
+  ]
+
+  @impl true
+  def new(attrs, opts \\ [])
+
+  def new(attrs, _opts) do
+    %__MODULE__{}
+    |> Mapping.build(attrs)
+    |> Mapping.parse(@mapping)
+  end
+end
---
+++ lib/horizon/offer.ex
@@ -0,0 +1,53 @@
+defmodule Stellar.Horizon.Offer do
+  @moduledoc """
+  Represents a `Offer` resource from Horizon API.
+  """
+
+  @behaviour Stellar.Horizon.Resource
+
+  alias Stellar.Horizon.Mapping
+
+  @type t :: %__MODULE__{
+          id: String.t(),
+          paging_token: String.t(),
+          seller: String.t(),
+          selling: map(),
+          buying: map(),
+          amount: float(),
+          price: float(),
+          price_r: map(),
+          last_modified_ledger: non_neg_integer(),
+          last_modified_time: DateTime.t(),
+          sponsor: String.t()
+        }
+
+  defstruct [
+    :id,
+    :paging_token,
+    :seller,
+    :selling,
+    :buying,
+    :amount,
+    :price,
+    :price_r,
+    :last_modified_ledger,
+    :last_modified_time,
+    :sponsor
+  ]
+
+  @mapping [
+    amount: :float,
+    price: :float,
+    last_modified_ledger: :integer,
+    last_modified_time: :date_time
+  ]
+
+  @impl true
+  def new(attrs, opts \\ [])
+
+  def new(attrs, _opts) do
+    %__MODULE__{}
+    |> Mapping.build(attrs)
+    |> Mapping.parse(@mapping)
+  end
+end
