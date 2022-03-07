d13096d5effc6df4d1ed4d6653cc26965bdd6466
Juan Hurtado <juan@kommit.co>
2022-03-07 18:03:18 -0500

    Horizon endpoints for Liquidity Pools (#121)

---
+++ test/horizon/liquidity_pools_test.exs
@@ -0,0 +1,286 @@
+defmodule Stellar.Horizon.Client.CannedLiquidityPoolRequests do
+  @moduledoc false
+
+  alias Stellar.Horizon.Error
+  alias Stellar.Test.Fixtures.Horizon
+
+  @base_url "https://horizon-testnet.stellar.org"
+
+  @spec request(
+          method :: atom(),
+          url :: String.t(),
+          headers :: list(),
+          body :: String.t(),
+          options :: list()
+        ) :: {:ok, non_neg_integer(), list(), String.t()} | {:error, atom()}
+  def request(
+        :get,
+        @base_url <>
+          "/liquidity_pools/0052d44a6c260660115f07c5a78631770e62aae3ffde96731c44b1509e9c8434",
+        _headers,
+        _body,
+        _opts
+      ) do
+    json_body = Horizon.fixture("liquidity_pool")
+    {:ok, 200, [], json_body}
+  end
+
+  def request(:get, @base_url <> "/liquidity_pools/not_found", _headers, _body, _opts) do
+    json_error = Horizon.fixture("404")
+    {:ok, 404, [], json_error}
+  end
+
+  def request(
+        :get,
+        @base_url <>
+          "/liquidity_pools/0052d44a6c260660115f07c5a78631770e62aae3ffde96731c44b1509e9c8434/transactions" <>
+          _query,
+        _headers,
+        _body,
+        _opts
+      ) do
+    json_body = Horizon.fixture("transactions")
+    {:ok, 200, [], json_body}
+  end
+
+  def request(
+        :get,
+        @base_url <>
+          "/liquidity_pools/0052d44a6c260660115f07c5a78631770e62aae3ffde96731c44b1509e9c8434/operations" <>
+          _query,
+        _headers,
+        _body,
+        _opts
+      ) do
+    json_body = Horizon.fixture("operations")
+    {:ok, 200, [], json_body}
+  end
+
+  def request(
+        :get,
+        @base_url <>
+          "/liquidity_pools/0052d44a6c260660115f07c5a78631770e62aae3ffde96731c44b1509e9c8434/effects" <>
+          _query,
+        _headers,
+        _body,
+        _opts
+      ) do
+    json_body = Horizon.fixture("effects")
+    {:ok, 200, [], json_body}
+  end
+
+  def request(
+        :get,
+        @base_url <>
+          "/liquidity_pools/0052d44a6c260660115f07c5a78631770e62aae3ffde96731c44b1509e9c8434/trades" <>
+          _query,
+        _headers,
+        _body,
+        _opts
+      ) do
+    json_body = Horizon.fixture("trades")
+    {:ok, 200, [], json_body}
+  end
+
+  def request(:get, @base_url <> "/liquidity_pools" <> _query, _headers, _body, _opts) do
+    json_body = Horizon.fixture("liquidity_pools")
+    {:ok, 200, [], json_body}
+  end
+end
+
+defmodule Stellar.Horizon.LiquidityPoolsTest do
+  use ExUnit.Case
+
+  alias Stellar.Horizon.Client.CannedLiquidityPoolRequests
+
+  alias Stellar.Horizon.{
+    Collection,
+    Effect,
+    Error,
+    LiquidityPool,
+    LiquidityPools,
+    Operation,
+    Trade,
+    Transaction
+  }
+
+  alias Stellar.Horizon.Operation.{CreateAccount, Payment, SetOptions}
+
+  setup do
+    Application.put_env(:stellar_sdk, :http_client, CannedLiquidityPoolRequests)
+
+    on_exit(fn ->
+      Application.delete_env(:stellar_sdk, :http_client)
+    end)
+
+    %{liquidity_pool_id: "0052d44a6c260660115f07c5a78631770e62aae3ffde96731c44b1509e9c8434"}
+  end
+
+  test "retrieve/1", %{liquidity_pool_id: liquidity_pool_id} do
+    {:ok,
+     %LiquidityPool{
+       fee_bp: 30,
+       id: "0052d44a6c260660115f07c5a78631770e62aae3ffde96731c44b1509e9c8434",
+       last_modified_ledger: 39_767_560,
+       last_modified_time: ~U[2022-02-24 11:48:09Z],
+       paging_token: "0052d44a6c260660115f07c5a78631770e62aae3ffde96731c44b1509e9c8434",
+       reserves: [
+         %{
+           amount: 522_256_061.074394,
+           asset: "XAU:GB3OE4IBQTYQFZZS5RXQHE4IPQL7ONOFOSAIS2NFRNMJKZEAI3AAUT2A"
+         },
+         %{
+           amount: 533_666_459.4045717,
+           asset: "ERRES:GA6ZAQGLDUEODDUUD3UD6PUFJYABWQA26SG5RK6E6CZ6OMD6AZKK5QNF"
+         }
+       ],
+       total_shares: 5.0e8,
+       total_trustlines: 1,
+       type: "constant_product"
+     }} = LiquidityPools.retrieve(liquidity_pool_id)
+  end
+
+  test "all/1" do
+    {:ok,
+     %Collection{
+       next:
+         "https://horizon-testnet.stellar.org/liquidity_pools?cursor=fd498c395920d57156e86b2d52acd2bbbfc164836a204c73d3b64bec33874d38&limit=3&order=desc",
+       prev:
+         "https://horizon-testnet.stellar.org/liquidity_pools?cursor=fe6490f5e64f25e62df49a6a4b5542b8e5411e84c6d3158613abb2271824deb9&limit=3&order=asc",
+       records: [
+         %LiquidityPool{
+           fee_bp: 30,
+           id: "fe6490f5e64f25e62df49a6a4b5542b8e5411e84c6d3158613abb2271824deb9",
+           last_modified_ledger: 1_137_275,
+           last_modified_time: ~U[2022-02-22 13:30:09Z],
+           reserves: [
+             %{
+               amount: 228.685116,
+               asset: "BTCN:GDGUZCQFXCLZ775VZH3YGWRUIBKD6XKDJEW3E7CE5COT4EO7A3W22YUX"
+             }
+             | _reserves1
+           ],
+           total_shares: 1.98,
+           total_trustlines: 1,
+           type: "constant_product"
+         },
+         %LiquidityPool{
+           fee_bp: 30,
+           id: "fdccc907a78a11e592c122144f702fdb093160c510c0f4327994d9be5d951092",
+           last_modified_ledger: 384_280,
+           last_modified_time: ~U[2022-01-07 18:19:08Z],
+           reserves: [
+             %{
+               amount: 100_005.015296,
+               asset: "DOLLAR:GCO7B6KEDWOBM5X642ZOTPYTYTTBZIGVGUED4ZSBILJOAU4XB7ISJBFF"
+             }
+             | _reserves2
+           ],
+           total_shares: 1.0e5,
+           total_trustlines: 1,
+           type: "constant_product"
+         },
+         %LiquidityPool{
+           fee_bp: 30,
+           id: "fd498c395920d57156e86b2d52acd2bbbfc164836a204c73d3b64bec33874d38",
+           last_modified_ledger: 973_360,
+           last_modified_time: ~U[2022-02-12 14:16:51Z],
+           reserves: [
+             %{amount: 130.8900524, asset: "native"}
+             | _reserves3
+           ],
+           total_shares: 180.893651,
+           total_trustlines: 4,
+           type: "constant_product"
+         }
+       ]
+     }} = LiquidityPools.all()
+  end
+
+  test "list_transactions/1", %{liquidity_pool_id: liquidity_pool_id} do
+    {:ok,
+     %Collection{
+       next:
+         "https://horizon.stellar.org/transactions?cursor=33736968114176\u0026limit=3\u0026order=asc",
+       prev:
+         "https://horizon.stellar.org/transactions?cursor=12884905984\u0026limit=3\u0026order=desc",
+       records: [
+         %Transaction{
+           id: "3389e9f0f1a65f19736cacf544c2e825313e8447f569233bb8db39aa607c8889",
+           ledger: 7840
+         },
+         %Transaction{
+           id: "2db4b22ca018119c5027a80578813ffcf582cda4aa9e31cd92b43cf1bda4fc5a",
+           ledger: 7841
+         },
+         %Transaction{
+           id: "3ce2aca2fed36da2faea31352c76c5e412348887a4c119b1e90de8d1b937396a",
+           ledger: 7855
+         }
+       ]
+     }} = LiquidityPools.list_transactions(liquidity_pool_id, limit: 3, order: :asc)
+  end
+
+  test "list_operations/1", %{liquidity_pool_id: liquidity_pool_id} do
+    {:ok,
+     %Collection{
+       next:
+         "https://horizon.stellar.org/transactions/132c440e984ab97d895f3477015080aafd6c4375f6a70a87327f7f95e13c4e31/operations?cursor=12884905985&limit=3&order=desc",
+       prev:
+         "https://horizon.stellar.org/transactions/132c440e984ab97d895f3477015080aafd6c4375f6a70a87327f7f95e13c4e31/operations?cursor=12884905987&limit=3&order=asc",
+       records: [
+         %Operation{
+           body: %SetOptions{},
+           type: "set_options",
+           type_i: 5
+         },
+         %Operation{
+           body: %Payment{},
+           type: "payment",
+           type_i: 1
+         },
+         %Operation{
+           body: %CreateAccount{},
+           type: "create_account",
+           type_i: 0
+         }
+       ]
+     }} = LiquidityPools.list_operations(liquidity_pool_id, limit: 3, order: :desc)
+  end
+
+  test "list_effects/1", %{liquidity_pool_id: liquidity_pool_id} do
+    {:ok,
+     %Collection{
+       next:
+         "https://horizon.stellar.org/effects?cursor=12884905985-3\u0026limit=3\u0026order=asc",
+       prev:
+         "https://horizon.stellar.org/effects?cursor=12884905985-1\u0026limit=3\u0026order=desc",
+       records: [
+         %Effect{type: "account_created", created_at: ~U[2015-09-30 17:15:54Z]},
+         %Effect{type: "account_debited", created_at: ~U[2015-09-30 17:16:54Z]},
+         %Effect{type: "signer_created", created_at: ~U[2015-09-30 17:17:54Z]}
+       ]
+     }} = LiquidityPools.list_effects(liquidity_pool_id, limit: 3)
+  end
+
+  test "list_trades/2", %{liquidity_pool_id: liquidity_pool_id} do
+    {:ok,
+     %Collection{
+       records: [
+         %Trade{base_amount: 4433.20},
+         %Trade{base_amount: 10.0},
+         %Trade{base_amount: 748.5338945}
+       ]
+     }} = LiquidityPools.list_trades(liquidity_pool_id)
+  end
+
+  test "error" do
+    {:error,
+     %Error{
+       extras: nil,
+       status_code: 404,
+       title: "Resource Missing",
+       type: "https://stellar.org/horizon-errors/not_found"
+     }} = LiquidityPools.retrieve("not_found")
+  end
+end
