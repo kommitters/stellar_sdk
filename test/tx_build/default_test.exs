8a5173717e84cb45c6604ab5a2d6582ee11dcfd1
Juan Hurtado <juan@kommit.co>
2022-04-14 11:27:33 -0500

    Update tests after changes

--- test/tx_build/default_test.exs
+++ test/tx_build/default_test.exs
@@ -19,14 +19,11 @@ defmodule Stellar.TxBuild.DefaultTest do
   }
 
   setup do
-    {public_key, secret} =
-      keypair =
-      KeyPair.from_secret_seed("SACHJRYLY43MUXRRCRFA6CZ5ZW5JVPPR4CWYWIX6BWRAOHOFVPVYDO5Z")
-
-    signature = Signature.new({public_key, secret})
+    keypair = KeyPair.from_secret_seed("SACHJRYLY43MUXRRCRFA6CZ5ZW5JVPPR4CWYWIX6BWRAOHOFVPVYDO5Z")
+    signature = Signature.new(keypair)
     source_account = Account.new("GD726E62G6G4ANHWHIQTH5LNMFVF2EQSEXITB6DZCCTKVU6EQRRE2SJS")
 
-    %TxBuild{tx: tx} = tx_build = TxBuild.new(source_account)
+    {:ok, %TxBuild{tx: tx}} = tx_build = TxBuild.new(source_account)
 
     %{
       source_account: source_account,
@@ -41,7 +38,7 @@ defmodule Stellar.TxBuild.DefaultTest do
   end
 
   test "new/2", %{source_account: source_account, tx: tx} do
-    %TxBuild{tx: ^tx, signatures: [], tx_envelope: nil} = TxBuild.new(source_account)
+    {:ok, %TxBuild{tx: ^tx, signatures: [], tx_envelope: nil}} = TxBuild.new(source_account)
   end
 
   test "new/2 with_options", %{source_account: source_account, keypair: {public_key, _secret}} do
@@ -56,14 +53,15 @@ defmodule Stellar.TxBuild.DefaultTest do
 
     operations = Operations.new([op])
 
-    %TxBuild{
-      tx: %Transaction{
-        sequence_number: ^sequence_number,
-        base_fee: ^base_fee,
-        memo: ^memo,
-        operations: ^operations
-      }
-    } =
+    {:ok,
+     %TxBuild{
+       tx: %Transaction{
+         sequence_number: ^sequence_number,
+         base_fee: ^base_fee,
+         memo: ^memo,
+         operations: ^operations
+       }
+     }} =
       TxBuild.new(
         source_account,
         sequence_number: sequence_number,
@@ -73,41 +71,92 @@ defmodule Stellar.TxBuild.DefaultTest do
       )
   end
 
-  test "new/2 with_bad_arguments", %{source_account: source_account} do
+  test "new/2 with_bad_options", %{source_account: source_account} do
     {:error, :invalid_time_bounds} = TxBuild.new(source_account, time_bounds: nil)
+  end
+
+  test "new/2 invalid_source_account" do
+    {:error, :invalid_source_account} = TxBuild.new("ABCD")
   end
 
   test "add_memo/2", %{tx_build: tx_build} do
     memo = Memo.new(text: "hello")
-    %TxBuild{tx: %Transaction{memo: ^memo}} = TxBuild.add_memo(tx_build, memo)
+    {:ok, %TxBuild{tx: %Transaction{memo: ^memo}}} = TxBuild.add_memo(tx_build, memo)
+  end
+
+  test "add_memo/2 invalid_memo", %{tx_build: tx_build} do
+    {:error, :invalid_memo} = TxBuild.add_memo(tx_build, "MEMO")
+  end
+
+  test "add_memo/2 piping_error" do
+    {:error, :invalid_source_account} = TxBuild.add_memo({:error, :invalid_source_account}, :memo)
   end
 
   test "set_time_bounds/2", %{tx_build: tx_build} do
     time_bounds = TimeBounds.new(min_time: 0, max_time: 123_456_789)
 
-    %TxBuild{tx: %Transaction{time_bounds: ^time_bounds}} =
+    {:ok, %TxBuild{tx: %Transaction{time_bounds: ^time_bounds}}} =
       TxBuild.set_time_bounds(tx_build, time_bounds)
+  end
+
+  test "set_time_bounds/2 invalid_time_bounds", %{tx_build: tx_build} do
+    {:error, :invalid_time_bounds} = TxBuild.set_time_bounds(tx_build, "12-10-2022")
+  end
+
+  test "set_time_bounds/2 piping_error" do
+    {:error, :invalid_memo} = TxBuild.set_time_bounds({:error, :invalid_memo}, :time_bounds)
   end
 
   test "set_sequence_number/2", %{tx_build: tx_build} do
     seq_number = SequenceNumber.new(123_456_789)
 
-    %TxBuild{tx: %Transaction{sequence_number: ^seq_number}} =
+    {:ok, %TxBuild{tx: %Transaction{sequence_number: ^seq_number}}} =
       TxBuild.set_sequence_number(tx_build, seq_number)
+  end
+
+  test "set_sequence_number/2 invalid_sequence_number", %{tx_build: tx_build} do
+    {:error, :invalid_sequence_number} = TxBuild.set_sequence_number(tx_build, 12_102_022)
+  end
+
+  test "set_sequence_number/2 piping_error" do
+    {:error, :invalid_memo} = TxBuild.set_sequence_number({:error, :invalid_memo}, :time_bounds)
   end
 
   test "set_base_fee/2", %{tx_build: tx_build} do
     base_fee = BaseFee.new(1_500)
 
-    %TxBuild{tx: %Transaction{base_fee: ^base_fee}} = TxBuild.set_base_fee(tx_build, base_fee)
+    {:ok, %TxBuild{tx: %Transaction{base_fee: ^base_fee}}} =
+      TxBuild.set_base_fee(tx_build, base_fee)
+  end
+
+  test "set_base_fee/2 invalid_base_fee", %{tx_build: tx_build} do
+    {:error, :invalid_base_fee} = TxBuild.set_base_fee(tx_build, 1_000)
+  end
+
+  test "set_base_fee/2 piping_error" do
+    {:error, :invalid_sequence_number} =
+      TxBuild.set_base_fee({:error, :invalid_sequence_number}, :base_fee)
   end
 
   test "add_operation/2", %{tx_build: tx_build, keypair: {public_key, _secret}} do
     op_body = CreateAccount.new(destination: public_key, starting_balance: 1.5)
     operation = Operation.new(op_body)
 
-    %TxBuild{tx: %Transaction{operations: %Operations{operations: [^operation]}}} =
+    {:ok, %TxBuild{tx: %Transaction{operations: %Operations{operations: [^operation]}}}} =
       TxBuild.add_operation(tx_build, op_body)
+  end
+
+  test "add_operation/2 invalid_operation", %{tx_build: tx_build} do
+    operation =
+      "INVALID_DESTINATION"
+      |> (&CreateAccount.new(destination: &1, starting_balance: 1.5)).()
+      |> Operation.new()
+
+    {:error, :invalid_operation} = TxBuild.add_operation(tx_build, operation)
+  end
+
+  test "add_operation/2 piping_error" do
+    {:error, :invalid_memo} = TxBuild.add_operation({:error, :invalid_memo}, :operation)
   end
 
   test "add_operations/2", %{tx_build: tx_build, keypair: {public_key, _secret}} do
@@ -115,7 +164,7 @@ defmodule Stellar.TxBuild.DefaultTest do
     op2 = Payment.new(destination: public_key, asset: :native, amount: 100)
     operations = [Operation.new(op1), Operation.new(op2)]
 
-    %TxBuild{tx: %Transaction{operations: %Operations{operations: ^operations}}} =
+    {:ok, %TxBuild{tx: %Transaction{operations: %Operations{operations: ^operations}}}} =
       TxBuild.add_operations(tx_build, [op1, op2])
   end
 
@@ -123,26 +172,50 @@ defmodule Stellar.TxBuild.DefaultTest do
     op1 = CreateAccount.new(destination: public_key, starting_balance: 1.5)
     op2 = Payment.new(destination: public_key, asset: :native, amount: 100)
 
-    %TxBuild{tx: %Transaction{base_fee: %BaseFee{fee: 200}, operations: %Operations{count: 2}}} =
+    {:ok,
+     %TxBuild{tx: %Transaction{base_fee: %BaseFee{fee: 200}, operations: %Operations{count: 2}}}} =
       TxBuild.add_operations(tx_build, [op1, op2])
+  end
+
+  test "add_operations/2 invalid_operation", %{tx_build: tx_build} do
+    operation =
+      "INVALID_DESTINATION"
+      |> (&CreateAccount.new(destination: &1, starting_balance: 1.5)).()
+      |> Operation.new()
+
+    {:error, :invalid_operation} = TxBuild.add_operations(tx_build, [operation])
+  end
+
+  test "add_operations/2 piping_error" do
+    {:error, :invalid_memo} = TxBuild.add_operations({:error, :invalid_memo}, :operation)
   end
 
   test "sign/2", %{keypair: keypair, tx_build: tx_build} do
     signature = Signature.new(keypair)
-    %TxBuild{signatures: [^signature | _signatures]} = TxBuild.sign(tx_build, signature)
+    {:ok, %TxBuild{signatures: [^signature | _signatures]}} = TxBuild.sign(tx_build, signature)
+  end
+
+  test "sign/2 invalid_signature", %{tx_build: tx_build} do
+    signature = Signature.new({"PUBLIC", "SECRET"})
+    {:error, :invalid_signature} = TxBuild.sign(tx_build, signature)
+  end
+
+  test "sign/2 piping_error" do
+    {:error, :invalid_operation} = TxBuild.sign({:error, :invalid_operation}, :signature)
   end
 
   test "sign/2 multiple", %{signature: signature, tx_build: tx_build} do
     {pk, sk} = KeyPair.random()
     signatures = [signature, Signature.new({pk, sk})]
 
-    %TxBuild{signatures: ^signatures} = TxBuild.sign(tx_build, signatures)
+    {:ok, %TxBuild{signatures: ^signatures}} = TxBuild.sign(tx_build, signatures)
   end
 
   test "sign_envelope/2", %{keypair: keypair, tx_envelope_base64: tx_envelope_base64} do
     signature = Signature.new(keypair)
 
-    "AAAAAgAAAAD/rxPaN43ANPY6ITP1bWFqXRISJdEw+HkQpqrTxIRiTQAAAGQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACxIRiTQAAAEDGQ1zlNXPps1aYpgCyHFzNgApPhKWhZqXlzPDMYXrZKilBt2SlWDkyki5pkiwKZ5Uc0bLNS1uqu31CJ5GFSWYOxIRiTQAAAEDGQ1zlNXPps1aYpgCyHFzNgApPhKWhZqXlzPDMYXrZKilBt2SlWDkyki5pkiwKZ5Uc0bLNS1uqu31CJ5GFSWYO" =
+    {:ok,
+     "AAAAAgAAAAD/rxPaN43ANPY6ITP1bWFqXRISJdEw+HkQpqrTxIRiTQAAAGQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACxIRiTQAAAEDGQ1zlNXPps1aYpgCyHFzNgApPhKWhZqXlzPDMYXrZKilBt2SlWDkyki5pkiwKZ5Uc0bLNS1uqu31CJ5GFSWYOxIRiTQAAAEDGQ1zlNXPps1aYpgCyHFzNgApPhKWhZqXlzPDMYXrZKilBt2SlWDkyki5pkiwKZ5Uc0bLNS1uqu31CJ5GFSWYO"} =
       TxBuild.sign_envelope(tx_envelope_base64, signature)
   end
 
@@ -152,12 +225,22 @@ defmodule Stellar.TxBuild.DefaultTest do
 
     signatures = [signature, Signature.new({pk, sk})]
 
-    "AAAAAgAAAAD/rxPaN43ANPY6ITP1bWFqXRISJdEw+HkQpqrTxIRiTQAAAGQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADxIRiTQAAAEDGQ1zlNXPps1aYpgCyHFzNgApPhKWhZqXlzPDMYXrZKilBt2SlWDkyki5pkiwKZ5Uc0bLNS1uqu31CJ5GFSWYOxIRiTQAAAEDGQ1zlNXPps1aYpgCyHFzNgApPhKWhZqXlzPDMYXrZKilBt2SlWDkyki5pkiwKZ5Uc0bLNS1uqu31CJ5GFSWYO8057hgAAAEC70Ava49XnFEQ6d9ed+IvfiMWL6do55bekG9LctPFnjTrRITSFs9cuHTfvbkSTCcFxw5IrZxqgeupuYb+ubU8H" =
+    {:ok,
+     "AAAAAgAAAAD/rxPaN43ANPY6ITP1bWFqXRISJdEw+HkQpqrTxIRiTQAAAGQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADxIRiTQAAAEDGQ1zlNXPps1aYpgCyHFzNgApPhKWhZqXlzPDMYXrZKilBt2SlWDkyki5pkiwKZ5Uc0bLNS1uqu31CJ5GFSWYOxIRiTQAAAEDGQ1zlNXPps1aYpgCyHFzNgApPhKWhZqXlzPDMYXrZKilBt2SlWDkyki5pkiwKZ5Uc0bLNS1uqu31CJ5GFSWYO8057hgAAAEC70Ava49XnFEQ6d9ed+IvfiMWL6do55bekG9LctPFnjTrRITSFs9cuHTfvbkSTCcFxw5IrZxqgeupuYb+ubU8H"} =
       TxBuild.sign_envelope(tx_envelope_base64, signatures)
   end
 
+  test "sign_envelope/2 invalid_signature", %{tx_envelope_base64: tx_envelope_base64} do
+    signature = Signature.new({"PUBLIC", "SECRET"})
+    {:error, :invalid_signature} = TxBuild.sign_envelope(tx_envelope_base64, signature)
+  end
+
   test "build/1", %{tx_build: tx_build, tx_envelope: tx_envelope} do
-    %TxBuild{tx_envelope: ^tx_envelope} = TxBuild.build(tx_build)
+    {:ok, %TxBuild{tx_envelope: ^tx_envelope}} = TxBuild.build(tx_build)
+  end
+
+  test "build/1 piping_error" do
+    {:error, :invalid_operation} = TxBuild.build({:error, :invalid_operation})
   end
 
   test "envelope/1", %{
@@ -165,9 +248,13 @@ defmodule Stellar.TxBuild.DefaultTest do
     tx_build: tx_build,
     tx_envelope_base64: tx_envelope_base64
   } do
-    ^tx_envelope_base64 =
+    {:ok, ^tx_envelope_base64} =
       tx_build
       |> TxBuild.sign(signature)
       |> TxBuild.envelope()
+  end
+
+  test "envelope/1 piping_error" do
+    {:error, :invalid_signature} = TxBuild.envelope({:error, :invalid_signature})
   end
 end
