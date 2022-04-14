29912aabbec3eaa917c0a467e64e6e36e1c39418
Juan Hurtado <juan@kommit.co>
2022-04-14 16:57:03 -0500

    Add missing spec

--- test/support/fixtures/xdr/liquidity_pools.ex
+++ test/support/fixtures/xdr/liquidity_pools.ex
@@ -13,9 +13,12 @@ defmodule Stellar.Test.Fixtures.XDR.LiquidityPools do
 
   @type pool_id :: String.t()
   @type amount :: integer()
+  @type price :: float()
   @type xdr :: PoolID.t() | OperationBody.t()
+
+  @spec liquidity_pool_id(pool_id :: pool_id()) :: PoolID.t()
   def liquidity_pool_id("929b20b72e5890ab51c24f1cc46fa01c4f318d8d33367d24dd614cfdf5491072") do
-    %StellarBase.XDR.PoolID{
+    %PoolID{
       value:
         <<48, 115, 234, 253, 12, 38, 185, 102, 73, 199, 191, 162, 150, 153, 210, 169, 215, 89, 64,
           243, 203, 7, 208, 93, 18, 82, 69, 194, 170, 58, 64, 125>>
