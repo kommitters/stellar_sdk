d0d6b9c6ea420749fb3fb5e744fea2d14b17224d
Juan Hurtado <juan@kommit.co>
2022-04-19 17:42:07 -0500

    Improve ClaimableBalanceID implementation

--- lib/tx_build/claimable_balance_id.ex
+++ lib/tx_build/claimable_balance_id.ex
@@ -7,27 +7,29 @@ defmodule Stellar.TxBuild.ClaimableBalanceID do
   @behaviour Stellar.TxBuild.XDR
 
   @type balance_id :: String.t() | binary()
+  @type balance_id_type :: :v0
 
-  @type t :: %__MODULE__{balance_id: balance_id()}
+  @type t :: %__MODULE__{balance_id: balance_id(), type: balance_id_type()}
 
-  defstruct [:balance_id]
+  defstruct [:balance_id, :type]
 
   @impl true
   def new(balance_id, opts \\ [])
 
-  def new(<<_disc::binary-size(8), _arm::binary-size(64)>> = balance_id, _opts)
+  def new(<<_balance_id_type::binary-size(8), balance_id::binary-size(64)>>, _opts)
       when is_bitstring(balance_id) do
-    %__MODULE__{balance_id: balance_id}
+    %__MODULE__{balance_id: balance_id, type: :v0}
   end
 
   def new(_balance_id, _opts), do: {:error, :invalid_balance_id}
 
   @impl true
-  def to_xdr(%__MODULE__{balance_id: balance_id}) do
+  def to_xdr(%__MODULE__{balance_id: balance_id, type: :v0}) do
     claimable_balance_id_type = ClaimableBalanceIDType.new(:CLAIMABLE_BALANCE_ID_TYPE_V0)
 
     balance_id
-    |> (&:crypto.hash(:sha256, &1)).()
+    |> String.upcase()
+    |> Base.decode16!()
     |> Hash.new()
     |> ClaimableBalanceID.new(claimable_balance_id_type)
   end
--- test/tx_build/claimable_balance_id_test.exs
+++ test/tx_build/claimable_balance_id_test.exs
@@ -1,8 +1,7 @@
 defmodule Stellar.TxBuild.ClaimableBalanceIDTest do
   use ExUnit.Case
 
-  import Stellar.Test.XDRFixtures, only: [claimable_balance_id_xdr: 1]
-
+  alias Stellar.Test.Fixtures.XDR, as: XDRFixtures
   alias Stellar.TxBuild.ClaimableBalanceID
 
   setup do
@@ -10,12 +9,15 @@ defmodule Stellar.TxBuild.ClaimableBalanceIDTest do
 
     %{
       balance_id: balance_id,
-      xdr: claimable_balance_id_xdr(balance_id)
+      xdr: XDRFixtures.claimable_balance_id(balance_id)
     }
   end
 
   test "new/2", %{balance_id: balance_id} do
-    %ClaimableBalanceID{balance_id: ^balance_id} = ClaimableBalanceID.new(balance_id)
+    %ClaimableBalanceID{
+      balance_id: "929b20b72e5890ab51c24f1cc46fa01c4f318d8d33367d24dd614cfdf5491072",
+      type: :v0
+    } = ClaimableBalanceID.new(balance_id)
   end
 
   test "new/2 invalid_balance_id" do
