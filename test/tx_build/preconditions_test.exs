38c1a2f883ef99aa47b62afa69bd8f03d8ce11f1
CristhianRodriguezMolina <camilo.r13@hotmail.com>
2022-08-09 10:46:06 -0500

    Added optional sequence number

--- test/tx_build/preconditions_test.exs
+++ test/tx_build/preconditions_test.exs
@@ -1,7 +1,14 @@
 defmodule Stellar.TxBuild.PreconditionsTest do
   use ExUnit.Case
 
-  alias Stellar.TxBuild.{Preconditions, TimeBounds, LedgerBounds, SequenceNumber, SignerKey}
+  alias Stellar.TxBuild.{
+    Preconditions,
+    TimeBounds,
+    LedgerBounds,
+    SequenceNumber,
+    SignerKey,
+    OptionalSequenceNumber
+  }
 
   alias StellarBase.XDR.{
     PreconditionsV2,
@@ -171,12 +178,14 @@ defmodule Stellar.TxBuild.PreconditionsTest do
       extra_signers: extra_signers,
       extra_signers_sdk: extra_signers_sdk
     } do
+      optional_min_seq_num = OptionalSequenceNumber.new(min_seq_num)
+
       %Preconditions{
         type: :precond_v2,
         preconditions: [
           time_bounds: ^time_bounds,
           ledger_bounds: ^ledger_bounds,
-          min_seq_num: ^min_seq_num,
+          min_seq_num: ^optional_min_seq_num,
           min_seq_age: ^min_seq_age,
           min_seq_ledger_gap: ^min_seq_ledger_gap,
           extra_signers: ^extra_signers_sdk
@@ -186,6 +195,36 @@ defmodule Stellar.TxBuild.PreconditionsTest do
           time_bounds: time_bounds,
           ledger_bounds: ledger_bounds,
           min_seq_num: min_seq_num,
+          min_seq_age: min_seq_age,
+          min_seq_ledger_gap: min_seq_ledger_gap,
+          extra_signers: extra_signers
+        )
+    end
+
+    test "Precondition type precond_v2 with min_seq_num nil", %{
+      time_bounds: time_bounds,
+      ledger_bounds: ledger_bounds,
+      min_seq_age: min_seq_age,
+      min_seq_ledger_gap: min_seq_ledger_gap,
+      extra_signers: extra_signers,
+      extra_signers_sdk: extra_signers_sdk
+    } do
+      optional_min_seq_num = OptionalSequenceNumber.new()
+
+      %Preconditions{
+        type: :precond_v2,
+        preconditions: [
+          time_bounds: ^time_bounds,
+          ledger_bounds: ^ledger_bounds,
+          min_seq_num: ^optional_min_seq_num,
+          min_seq_age: ^min_seq_age,
+          min_seq_ledger_gap: ^min_seq_ledger_gap,
+          extra_signers: ^extra_signers_sdk
+        ]
+      } =
+        Preconditions.new(
+          time_bounds: time_bounds,
+          ledger_bounds: ledger_bounds,
           min_seq_age: min_seq_age,
           min_seq_ledger_gap: min_seq_ledger_gap,
           extra_signers: extra_signers
@@ -223,6 +262,8 @@ defmodule Stellar.TxBuild.PreconditionsTest do
       min_seq_ledger_gap: min_seq_ledger_gap,
       extra_signers_sdk: extra_signers_sdk
     } do
+      optional_min_seq_num = OptionalSequenceNumber.new(min_seq_num)
+
       %PreconditionsXDR{
         type: %PreconditionType{identifier: :PRECOND_V2},
         preconditions: %PreconditionsV2{
@@ -262,7 +303,61 @@ defmodule Stellar.TxBuild.PreconditionsTest do
           preconditions: [
             time_bounds: time_bounds,
             ledger_bounds: ledger_bounds,
-            min_seq_num: min_seq_num,
+            min_seq_num: optional_min_seq_num,
+            min_seq_age: min_seq_age,
+            min_seq_ledger_gap: min_seq_ledger_gap,
+            extra_signers: extra_signers_sdk
+          ]
+        })
+    end
+
+    test "Preconditions type precond_v2 with min_seq_num nil", %{
+      time_bounds: time_bounds,
+      ledger_bounds: ledger_bounds,
+      min_seq_age: min_seq_age,
+      min_seq_ledger_gap: min_seq_ledger_gap,
+      extra_signers_sdk: extra_signers_sdk
+    } do
+      optional_min_seq_num = OptionalSequenceNumber.new()
+
+      %PreconditionsXDR{
+        type: %PreconditionType{identifier: :PRECOND_V2},
+        preconditions: %PreconditionsV2{
+          time_bounds: %OptionalTimeBounds{time_bounds: nil},
+          ledger_bounds: %OptionalLedgerBounds{ledger_bounds: nil},
+          min_seq_num: %OptionalSequenceNumberXDR{sequence_number: nil},
+          min_seq_age: %Duration{value: 1500},
+          min_seq_ledger_gap: %UInt32{datum: 2500},
+          extra_signers: %SignerKeyList{
+            signer_keys: [
+              %SignerKeyXDR{
+                signer_key: %UInt256{
+                  datum:
+                    <<111, 94, 211, 67, 247, 211, 243, 210, 220, 210, 84, 19, 15, 150, 110, 92,
+                      222, 73, 207, 189, 198, 89, 226, 31, 8, 156, 110, 212, 64, 137, 133, 22>>
+                },
+                type: %SignerKeyType{
+                  identifier: :SIGNER_KEY_TYPE_ED25519
+                }
+              },
+              %StellarBase.XDR.SignerKey{
+                signer_key: %StellarBase.XDR.UInt256{
+                  datum:
+                    <<166, 253, 99, 166, 207, 231, 51, 29, 108, 218, 82, 161, 161, 241, 223, 129,
+                      248, 20, 182, 229, 112, 154, 211, 208, 111, 24, 212, 20, 168, 1, 184, 145>>
+                },
+                type: %StellarBase.XDR.SignerKeyType{identifier: :SIGNER_KEY_TYPE_HASH_X}
+              }
+            ]
+          }
+        }
+      } =
+        Preconditions.to_xdr(%Preconditions{
+          type: :precond_v2,
+          preconditions: [
+            time_bounds: time_bounds,
+            ledger_bounds: ledger_bounds,
+            min_seq_num: optional_min_seq_num,
             min_seq_age: min_seq_age,
             min_seq_ledger_gap: min_seq_ledger_gap,
             extra_signers: extra_signers_sdk
