a72d50cabdd34500d90e2360bd7340662af7600c
CristhianRodriguezMolina <50219561+CristhianRodriguezMolina@users.noreply.github.com>
2023-10-27 10:10:52 -0500

    Update dependency url

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule Stellar.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:stellar_base, git: "git@github.com:kommitters/stellar_base.git", branch: "v0.15"},
+      {:stellar_base, git: "https://github.com/kommitters/stellar_base.git", branch: "v0.15"},
       {:ed25519, "~> 1.3"},
       {:hackney, "~> 1.17"},
       {:jason, "~> 1.0"},
