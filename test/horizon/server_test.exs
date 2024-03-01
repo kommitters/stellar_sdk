70543eb10544dcaa55fe1c409a80610d0d4c1e90
Miguel Nieto <39246879+miguelnietoa@users.noreply.github.com>
2024-03-01 00:00:00 -0500

    Add tests for new Server struct

---
+++ test/horizon/server_test.exs
@@ -0,0 +1,25 @@
+defmodule Stellar.Horizon.ServerTest do
+  use ExUnit.Case
+
+  alias Stellar.Horizon.Server
+
+  test "new/1" do
+    %Server{url: "https://standalone-horizon.com"} = Server.new("https://standalone-horizon.com")
+  end
+
+  test "public/0" do
+    %Server{url: "https://horizon.stellar.org"} = Server.public()
+  end
+
+  test "testnet/0" do
+    %Server{url: "https://horizon-testnet.stellar.org"} = Server.testnet()
+  end
+
+  test "futurenet/0" do
+    %Server{url: "https://horizon-futurenet.stellar.org"} = Server.futurenet()
+  end
+
+  test "local/0" do
+    %Server{url: "http://localhost:8000"} = Server.local()
+  end
+end
